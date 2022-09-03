328b845cf179a765225dcf1c7a7444032c132b54
ankit951github <mishrag6393@gmail.com>
2022-09-03 17:11:22 +0530

    Adding conditional Assignment file

---
+++ Conditional assignment problems/problem3.js
@@ -0,0 +1,15 @@
+// Problem 3: Given 2 numbers a and b print which is greater or "both equal".
+
+// let num1 = 10; num2 = 10;
+
+// let num1 = 10; num2 = 15;
+let num1 = 10; num2 = 8;
+if(num1>num2){
+  console.log(num1);
+}
+else if(num1<num2){
+  console.log(num2);
+}
+else{
+  console.log("both equal");
+}
---
+++ Conditional assignment problems/problem4.js
@@ -0,0 +1,20 @@
+// Problem 4: Given stored username and password and input username and password, Print if the user can login or not.
+
+// let givenUserName = "ankit123";
+// let givenPassword = "Ankit";
+
+// let inputUserName = "ankit123";
+// let inputPassword = "Ankit";
+
+// let inputUserName = "ankit123";
+// let inputPassword = "Ankit6654";
+
+let inputUserName = "ankit1234";
+let inputPassword = "Ankit";
+
+if(givenUserName == inputUserName && givenPassword === inputPassword){
+  console.log("User can login");
+}
+else{
+  console.log("User can not login");
+}
