4890a2bbb4d508fdb825883363c06f7d23607bdc
ankit951github <mishrag6393@gmail.com>
2022-09-03 17:15:45 +0530

    addign logical opraters assingment file

---
+++ Logical_statmaents_Assignments03092022/problem4.js
@@ -0,0 +1,4 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+let num1=3, num2=5, num3=9;
+
+num1>num2 && num1>num3? console.log(num1): num2>num1 && num2>num3? console.log(num2): console.log(num3);
