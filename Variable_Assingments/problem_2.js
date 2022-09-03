931318b181fe648d8d69f236240764759e8fa8f8
ankit951github <mishrag6393@gmail.com>
2022-09-03 17:02:25 +0530

    Adding Variable Assingment problems

---
+++ Variable_Assingments/problem_2.js
@@ -0,0 +1,6 @@
+let name = "Ankit Mishra";
+console.log(name);
+name = "Rajendra Mishra";
+console.log(name);
+name = "Uma Devi";
+console.log(name);
---
+++ Variable_Assingments/problem_3.js
@@ -0,0 +1,6 @@
+let name = "Ankit Mishra";
+let age = 26;
+console.log(name);
+console.log(typeof(name));
+console.log(age);
+console.log(typeof(age))
